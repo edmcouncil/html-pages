529032a19b411eccd5792d43796506ffa3fd51f0
Robert Trypuz <robert.trypuz@makolab.com>
2020-06-04 10:13:01 +0200

    Harsh's slightly revised version of content for OKGL web site
    Signed-off-by: Robert Trypuz <robert.trypuz@makolab.com>

--- home/src/views/Home.vue
+++ home/src/views/Home.vue
@@ -11,23 +11,29 @@
 <h4>Why Graphs</h4> 
 
 <h5>This is the world we living</h5>
-<p  class="text">We live in a hyper-connected world where an invisible bio-adversary can deliver a toxic shock to our global system with a largely unmeasurable impact on our Lives and Livelihood. As businesses strive to understand the Risk impact, assess their business resiliency and pivot to growth, we are struggling to stitch together holistic, continuous, and contextually relevant Intelligence at a scale and speed we need.</p>
+<p  class="text">We live in a hyper-connected world where an invisible bio-adversary can deliver a toxic shock to our global system with a largely unmeasurable impact on our Lives and Livelihood. As businesses strive to understand the Risk impact, assess their business resiliency and pivot to growth, we are unable to  stitch together holistic, continuous, and contextually relevant Intelligence at a scale and speed we need to deal with the 'New Normal'.</p>
+
+<p class="text">Per IDC estimation, in 2020, we’ll produce and consume more than 59 Zettabytes of data with 26% growth over next 3 years and a larger portion of this will be un-structured data. While we have made tremendous progress in applying machine intelligence to understand and derive Insights from our internal data, our data ecosystems remain disjointed with significant lack of Trust. Our current architectures and technology are not able to connect and integrate internal and external data to enable Agile Intelligence we need. This further inhibits our ability to develop newer Products and Services,  maintain and grow a trusted relationship with our customers and partners while lowering our Risk profile.</p>
+
+<h5>What we urgently need</h5>
+
+<p class="text">What we urgently need is new strategy, architecture, and solutions to develop the Agile Intelligence. <b>Knowledge Graphs</b> are well suited to serve as the DNA (core?) of this new architecture. <b>Knowledge Graphs mimic how humans brain thinks – in terms of connections and relationships</b>. Adding advanced machine intelligence to the underlying Graph databases enables the Enterprise Digital Brain that can deliver the <b>enterprise level Agile Intelligence</b>.</p>
 
 
+<h4>Knowledge Graph Market Size and Growth Trajectory</h4>
+<p class="text">Per Marketsandmarkets, Knowledge Graph industry is poised to experience hyper-growth ($3B and 23% CAGR by 2023). Other industry analysts such as Gartner are bullish on the Knowledge Graph market trends. <b>Gartner lists Graph powered augmented analytics to be among top 10 trends with 30% of the global organizations making Graph powered decisions</b>.</p>
 
-
-<p class="text">Per IDC estimation, in 2020 we’ll produce and consume more than 59 Zettabytes of data with 26% growth over next 3 years and a larger portion of this will be un-structured data. While we have made tremendous progress in applying machine intelligence to understand and derive Insights from our internal data, our data ecosystems remain disjointed with significant lack of Trust. Our current architectures and technology are not able to connect and integrate internal and external data to enable Intelligence we need. This further inhibits our ability to develop newer Products and Services and maintain and grow a trusted relationship with our customers and partners while lowering our Risk profile.</p>
-
-<h5>What we really need</h5>
-
-<p class="text">What we really need is new strategy, architecture, and solutions to develop the Agile Intelligence we urgently need. <b>Knowledge Graphs</b> are well suited to serve as the DNA of this new architecture. Knowledge Graphs mimic how humans brain thinks – in terms of connections and relationships. Adding advanced machine intelligence to the underlying Graph databases enables the Enterprise Digital Brain.</p>
-
-
-<h4>Market Size, Growth Trajectory</h4>
-<p class="text">Per Marketsandmarkets, Knowledge Graph market is poised to experience hyper-growth ($3B and 23% CAGR by 2023). Other industry analysts such as Gartner are bullish on the Knowledge Graph market trends. <b>Gartner lists Graph powered augmented analytics to be among top 10 trends with 30% of the global organizations making Graph powered decisions</b>. </p>
-
-<h5>Key use cases of Knowledge Graphs</h5>
-<p class="text">Key use cases of Knowledge Graphs range from Risk, Fraud, Recommendation Engines, Supply-Chain analytics, Customer analysis. Industries such as Financial Services, Insurance, Life-Sciences, Healthcare are leading the way. Cloud based Knowledge Graph applications Marketplaces are in development.</p>
+<h5>Knowledge Graphs use cases</h5>
+<p class="text">Key use cases of Knowledge Graphs include 
+<ul>
+    <li>Risk</li>
+    <li>Fraud</li>
+    <li>Recommendation Engines</li>
+    <li>IoT streaming analytics</li>
+    <li>Supply-Chain analytics and</li>
+    <li>Customer analysis</li>
+</ul>
+Industries such as Financial Services, Insurance, Life-Sciences, Healthcare are leading the way. Cloud based Knowledge Graph applications Marketplaces are in development.</p>
 
 <h4>EDMC Advocates Knowledge Graph Adoption</h4> 
 
