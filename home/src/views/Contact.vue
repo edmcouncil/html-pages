60aeabf2dfd943587f305764c84081bbdd58d42e
Robert Trypuz <robert.trypuz@makolab.com>
2020-06-12 10:58:12 +0200

    OKG contact persons

--- home/src/views/Contact.vue
+++ home/src/views/Contact.vue
@@ -1,60 +1,138 @@
 <template>
-  <div class="container">
-    <article>
-      <h1>
-        <span>Contact Person</span>
-      </h1>
-
-                <h4><span>Michael Meriton, EDMC Co-Founder & COO</span></h4>
-<img class="img-avatar" src="../assets/img/edm_staff_michael_meriton.png" alt="Harsh Sharma, PhD" align="left" />                <ul>
-                    <li>Mike is a Co-Founder of the EDM Council and served as the first Chairman and active Board member since inception in 2005. </li>
-                    <li>Mike joined in 2015 as a Senior Advisor, promoted to COO in 2020, to lead Industry Engagement strategy, new member services and Council Operations. </li>
-                    <li>Previously, Mike was the CEO of GoldenSource and held key executive roles at CheckFree (Fiserv), D&B and Oracle.</li>
-                </ul>      
-
-
-
- 
-
-
-
-
-                      
-      <p class="text">
-        <a
-          class="bttn"
-          id="email_1524650877557"
-          href="mailto:mmeriton@edmcouncil.org?subject=OKGL"
-        ><b>Contact Michael Meriton</b></a> to learn more about OKG initiative and OKG training opportunities.</p><br><br>
+   <div class="container">
+      <article>
+         <h1>
+            <span>OKG Lab Contact Person</span>
+         </h1>
+         <h5><span>Michael Meriton, EDMC Co-Founder & COO</span></h5>
+         <img class="img-avatar" src="../assets/img/edm_staff_michael_meriton.png" alt="Michael Meriton, EDMC Co-Founder & COO<" align="left" />                
+         <ul>
+            <li>Mike is a Co-Founder of the EDM Council and served as the first Chairman and active Board member since inception in 2005. </li>
+            <li>Mike joined in 2015 as a Senior Advisor, promoted to COO in 2020, to lead Industry Engagement strategy, new member services and Council Operations. </li>
+            <li>Previously, Mike was the CEO of GoldenSource and held key executive roles at CheckFree (Fiserv), D&B and Oracle.</li>
+         </ul>
+         <p class="text">
+            <a
+               class="bttn"
+               id="email_1524650877557"
+               href="mailto:mmeriton@edmcouncil.org?subject=OKGL"
+               ><b>Contact Michael Meriton</b></a> to learn more about OKG initiative.
+         </p>
+         <br><br>
 
 
-<!-- Contacts
-Robert Trypuz
-Dean Allemang
-Harsh Sharma
-Robert Rummel -->
+         <h1>
+            <span>OKG training</span>
+         </h1>
+         <h5><span>Colin Gibson, EMEA Regional Advocate</span></h5>
+         <img class="img-avatar" src="../assets/img/edm_staff_colin_gibson.jpg" alt="Colin Gibson, EMEA Regional Advocate" align="left" />                
+         <ul>
+            <li>Colin is a senior executive and consultant with 30 years’ experience in financial services. He specialises in architecture, data governance and data management, most recently as head of data architecture for the investment banking divisions of RBS and HSBC and as head of enterprise architecture for Willis Towers Watson. </li>
+            <li>Colin combines an enthusiasm for all things data with experience in software development, technology operations and management of major change programmes.</li>
+         </ul>
+         <p class="text">
+            <a
+               class="bttn"
+               id="email_1524650877557"
+               href="mailto:cgibson@edmcouncil.org?subject=Question about the OKG Training"
+               ><b>Contact Colin Gibson</b></a> to learn more about Open Knowledge Graph training opportunities.
+         </p>
+         <br><br>
 
-    </article>
-  </div>
+
+         <h5><span>Michael O'Sullivan, APAC Regional Advocate Australia & New Zealand</span></h5>
+         <img class="img-avatar" src="../assets/img/edm_staff_michael_osullivan.png" alt="Michael O'Sullivan, APAC Regional Advocate Australia & New Zealand" align="left" />                
+         <ul>
+            <li>Michael is a Regional Advocate for the Asia Pacific (APAC) Region. </li>
+            <li>Michael is passionate about all things data and has spent his career working in the data & analytics domain for companies in Europe, the Middle East and Asia Pacific.</li>
+            <li>Michael is based in Sydney, Australia where he works as an independent data management consultant. Michael assists the EDM Council on a part-time basis and provides on the ground support to our Australian and New Zealand membership.</li>
+         </ul>
+         <p class="text">
+            <a
+               class="bttn"
+               id="email_1524650877557"
+               href="mailto:mosullivan@edmcouncil.org?subject=Question about the OKG Training"
+               ><b>Contact Michael O'Sullivan</b></a> to learn more about Open Knowledge Graph training opportunities.
+         </p>
+         <br><br>
+
+
+         <h5><span>David Chew, APAC Regional Advocate Singapore</span></h5>
+         <img class="img-avatar" src="../assets/img/edm_staff_david_chew.png" alt="David Chew, APAC Regional Advocate Singapore" align="left" />                
+         <ul>
+            <li>David is the Regional Advocate and evangelist for the Asia Pacific (APAC) Region. David has over 20 years of enterprise solutions and data management experience supporting Financial Institutions and Large Enterprises cross Industry.</li>
+            <li>David is based in Singapore and will assist the EDM Council on a part-time basis with membership activities and initiatives in the APAC region.</li>
+         </ul>
+         <p class="text">
+            <a
+               class="bttn"
+               id="email_1524650877557"
+               href="mailto:dchew@edmcouncil.org?subject=Question about the OKG Training"
+               ><b>Contact David Chew</b></a> to learn more about Open Knowledge Graph training opportunities.
+         </p>
+         <br><br>        
+
+         <h1>
+            <span>Phone numbers</span>
+         </h1><br>
+
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <th>Contact Purpose</th>
+                            <th>Name</th>
+                            <th>EDMC Role</th>
+                            <th>Phone Number</th>
+                        </tr>
+                        <tr>
+                            <td>OKG Lab</td>
+                            <td><b>Michael Meriton</b></td>
+                            <td>EDMC Co-Founder & COO</td>
+                            <td>+1 908 720 7044</td>
+                        </tr>
+                        <tr>
+                            <td>OKG Training</td>
+                            <td><b>Colin Gibson</b></td>
+                            <td>EMEA Regional Advocate</td>
+                            <td>+44 (0)7875 365022</td>
+                        </tr>
+                        <tr>
+                            <td>OKG Training</td>
+                            <td><b>David Chew</b></td>
+                            <td>APAC Regional Advocate, Singapore</td>
+                            <td>+65 8186 8708</td>
+                        </tr>
+                        <tr>
+                            <td>OKG Training</td>
+                            <td><b>Michael O'Sullivan</b></td>
+                            <td>APAC Regional Advocate, Australia & New Zealand</td>
+                            <td>+61 415 652 507</td>
+                        </tr>
+                    </tbody>
+                </table><br>                   
+         <!-- Contacts
+            Robert Trypuz
+            Dean Allemang
+            Harsh Sharma
+            Robert Rummel -->
+      </article>
+   </div>
 </template>
-
-
 <script>
-export default {
-  name: 'contact',
-  components: {},
-};
+   export default {
+     name: 'contact',
+     components: {},
+   };
 </script>
-
 <style lang="scss" scoped>
-.img-avatar{
-    width: 200px;
-    height: 200px;
-    background-size: cover;
-    display: block;
-    border-radius: 100px;
-    -webkit-border-radius: 100px;
-    margin: 0px 30px;
-    -moz-border-radius: 100px;
-}
+   .img-avatar{
+   width: 200px;
+   height: 200px;
+   background-size: cover;
+   display: block;
+   border-radius: 100px;
+   -webkit-border-radius: 100px;
+   margin: 0px 30px;
+   -moz-border-radius: 100px;
+   }
 </style>
