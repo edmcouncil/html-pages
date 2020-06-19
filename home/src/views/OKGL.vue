923dce95c9483824a24dce6cc6206619132fc140
Robert Trypuz <robert.trypuz@makolab.com>
2020-06-19 13:21:31 +0200

    Typos corrected.
    Signed-off-by: Robert Trypuz <robert.trypuz@makolab.com>

--- home/src/views/OKGL.vue
+++ home/src/views/OKGL.vue
@@ -16,31 +16,31 @@
 <h4>OKGL Mission, Participants</h4>
 
 
-<p class="text">EDMC recognizes the urgent need to bring this new architecture and approach to its members and beyond. In consultations with various members, analysts, and software vendors we have launched the Open Knowledge Graph Lab (OKGL). OKGL mission is to:</p>
+<p class="text">EDMC recognizes the urgent need to bring this new architecture and approach to its members and beyond. In consultations with various members, analysts, and software vendors, we have launched the Open Knowledge Graph Lab (OKGL). OKGL mission is to:</p>
 <ul>
     <li>(1) Establish and grow a global community of Knowledge Graph Consumers, makers of technology, enablers such as management consultancies and Systems Integrators and industry standards bodies to promote enterprise-wide adoption of this technology.</li>
-    <li>(2) Develop open source, re-usable, Plug and Play knowledge Graphs of high mutualized value via a Cloud marketplace with Sandbox to test drive use-cases in an accelerated manner.</li>
-    <li>(3) Provide training and mentorship to participants on Knowledge Graph architectures, supporting Ontology development while promoting adoption of relevant industry standards.</li>
+    <li>(2) Develop open-source, re-usable, Plug and Play knowledge Graphs of high mutualized value via a Cloud marketplace with Sandbox to test drive use-cases in an accelerated manner.</li>
+    <li>(3) Provide training and mentorship to participants on Knowledge Graph architectures, supporting Ontology development while promoting the adoption of relevant industry standards.</li>
     <li>(4) Develop and provide industry benchmarks to demonstrate adoption across various industries.</li>            
 </ul>
 
 <h4>OKGL roll-out</h4>
   
 <h5>Q3, 2020: Phase 1 – Training, Ontology Reviews, POV on Graph Architectures and Awareness</h5>
-<p class="text">OKGL is already serving early-adopter customers with Knowledge Graph training, Ontology reviews and ability to further refine domain specific Ontologies. Where applicable, OKGL is also leveraging its FIBO Ontology for broader Fraud and Risk use cases.</p>
+<p class="text">OKGL is already serving early-adopter customers with Knowledge Graph training, Ontology reviews, and ability to refine domain-specific Ontologies further. Where applicable, OKGL is also leveraging its FIBO Ontology for broader Fraud and Risk use cases.</p>
 
 <h5>Q3-4, 2020: Phase 2 – Mutualized value Knowledge Graph Development and Socialization</h5> 
-<p class="text">Establish working group to identify and develop 1st mutualized value Knowledge Graph use case. Early candidates include AML, Fraud leveraging FIBO standard. This working group will also develop the prototype to demonstrate how AML Knowledge Graph will help address the challenges in this space more efficiently than current techniques.</p>
+<p class="text">Establish a working group to identify and develop 1st mutualized value Knowledge Graph use case. Early candidates include AML, Fraud leveraging FIBO standards. This working group will also develop the prototype to demonstrate how AML Knowledge Graph will help address the challenges in this space more efficiently than current techniques.</p>
 
 <h5>Q4, 2020-Q1, 2021: Phase 3 – Additional Knowledge Graphs, Cloud Sandbox availability</h5>
-<p class="text">Due to intense focus on Covid-19 crisis, 2nd working group will focus on developing Covid-19 Knowledge Graph. Additional work groups on use cases such as supply-chain and workplace and worker safety are some additional candidates.</p>
+<p class="text">Due to intense focus on the Covid-19 crisis, 2nd working group will focus on developing the Covid-19 Knowledge Graph. Additional workgroups on use cases such as supply-chain and workplace and worker safety are some additional candidates.</p>
 
 
 <h4>Cloud Sandbox</h4>
-<p class="text">OKGL will host a Cloud Sandbox that will serve as a testbed for Consumers to rapidly develop prototypes and performance metrics. This capability will be enabled by technology vendors and consultancies.</p> 
+<p class="text">OKGL will host a Cloud Sandbox that will serve as a testbed for Consumers to develop prototypes and performance metrics rapidly. This capability will be enabled by technology vendors and consultancies.</p> 
 
 <h4>Partnership</h4>
-<p class="text">OKGL will also establish partnership with other ecosystems such as R3 and hashgraph to integrate power of Blockchain and Hashgraph architectures and techniques to further enhance value of Knowledge Graph powered Insights. </p> 
+<p class="text">OKGL will also establish partnerships with other ecosystems such as R3 and hashgraph to integrate the power of Blockchain and Hashgraph architectures and techniques to further enhance the value of Knowledge Graph powered Insights. </p> 
 
 
 
