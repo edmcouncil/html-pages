0e2060afb163897debdb44098efe0101a5f077fc
dasawanaka <michal.19.daniel.96@gmail.com>
2021-10-19 05:25:31 +0200

    Show more/less on every level in enumeration implemented in auto and fibo pages
    Signed-off-by: dasawanaka <michal.19.daniel.96@gmail.com>

--- auto/src/components/chunks/AXIOM.vue
+++ auto/src/components/chunks/AXIOM.vue
@@ -1,34 +1,92 @@
 <template>
-  <component v-bind:is="processedHtml"></component>
-</template>
-<script>
-import Vue from 'vue';
-import customLink from './link';
+<div v-if="!isShowMore">
+    <component v-bind:is="fullProcessedHtml"></component>
+</div>
+<div v-else>
+    <component v-bind:is="sliceProcessedHtml">
+    </component>
+    <a href="#" 
+      v-show="!isMoreVisible" 
+      @click.prevent="isMoreVisible = !isMoreVisible">
+        See more...
+    </a>
 
-Vue.component('customLink', customLink);
+    <div v-show="isMoreVisible">
+        <component v-bind:is="moreProcessedHtml"></component>
+        <a href="#" 
+          @click.prevent="isMoreVisible = !isMoreVisible">
+            See less...
+        </a>
+    </div>
+</div>
+</template>
+
+<script>
+import Vue from "vue";
+import customLink from "./link";
+
+Vue.component("customLink", customLink);
 
 export default {
-  name: 'AXIOM',
-  components: {
-    customLink,
-  },
-  props: ['value', 'entityMaping'],
-  computed: {
-    processedHtml() {
-      let html = this.value;
-      if (this.entityMaping) {
-        Object.keys(this.entityMaping).forEach((name) => {
-          const value = this.entityMaping[name];
-          html = html.replace(
-            name,
-            `<customLink name="${value.label}" query="${value.iri}"></customLink>`,
-          );
-        });
-      }
-      return {
-        template: `<div>${html}</div>`,
-      };
+    name: "AXIOM",
+    components: {
+        customLink
     },
-  },
+    props: ["value", "entityMaping"],
+    computed: {
+        fullProcessedHtml() {
+            let html = this.processedHtml(this.value);
+            return {
+                template: `<div>${html}</div>`
+            };
+        },
+        sliceProcessedHtml() {
+            let html = this.processedHtml(this.lines.slice(0, 6).join("<br />"));
+            return {
+                template: `<div>${html}</div>`
+            };
+        },
+        moreProcessedHtml() {
+            let html = this.processedHtml(this.lines.slice(6).join("<br />"));
+            return {
+                template: `<div>${html}</div>`
+            };
+        }
+    },
+    data() {
+        let html = this.value;
+        let lines = html.split(/(?:\r\n|\r|\n)/g);
+        if (lines.length == 1) {
+            lines = html.split("<br />");
+        }
+        return {
+            lines: lines,
+            isShowMore: false,
+            isMoreVisible: false
+        };
+    },
+
+    mounted() {
+        if (this.lines.length > 6) {
+            // yes 6, first line is "title"
+            this.isShowMore = true;
+        }
+    },
+    methods: {
+        processedHtml: function (htmlInput) {
+            let htmlResult = htmlInput;
+            htmlResult = htmlResult.replace("/arg1/", "<b>/arg1/</b>");
+            if (this.entityMaping) {
+                Object.keys(this.entityMaping).forEach(name => {
+                    const value = this.entityMaping[name];
+                    htmlResult = htmlResult.replace(
+                        name,
+                        `<customLink name="${value.label}" query="${value.iri}"></customLink>`
+                    );
+                });
+            }
+            return htmlResult;
+        }
+    }
 };
 </script>
--- fibo/src/components/chunks/STRING.vue
+++ fibo/src/components/chunks/STRING.vue
@@ -1,59 +1,46 @@
 <template>
-  <div>
-    <div
-      v-if="!isShowMore"
-      v-html="lines.join('<br />')"
-    />
+<div>
+    <div 
+      v-if="!isShowMore" 
+      v-html="lines.join('<br />')" />
 
-    <div
-      v-else
-    >
-      <div
-        v-html="lines.slice(0, 6).join('<br />')"
-      />
-      <a
-        href="#"
-        v-show="!isMoreVisible"
-        @click.prevent="isMoreVisible = !isMoreVisible"
-      >
-        See more...
-      </a>
+    <div v-else>
+        <div v-html="lines.slice(0, 6).join('<br />')" />
+        <a href="#" 
+          v-show="!isMoreVisible" 
+          @click.prevent="isMoreVisible = !isMoreVisible">
+            See more...
+        </a>
 
-      
+        <div v-show="isMoreVisible" 
+          v-html="lines.slice(6).join('<br />')" />
 
-      <div
-        v-show="isMoreVisible"
-        v-html="lines.slice(6).join('<br />')"
-      />
-
-      <a
-        href="#"
-        v-show="isMoreVisible"
-        @click.prevent="isMoreVisible = !isMoreVisible"
-      >
-        See less...
-      </a>
-
+        <a href="#" 
+          v-show="isMoreVisible" 
+          @click.prevent="isMoreVisible = !isMoreVisible">
+            See less...
+        </a>
     </div>
-  </div>
+</div>
 </template>
+
 <script>
 export default {
-  name: 'STRING',
-  props: ['value'],
+    name: 'STRING',
+    props: ['value'],
 
-  data() {
-    return {
-      lines: this.value.split(/(?:\r\n|\r|\n)/g),
-      isShowMore: false,
-      isMoreVisible: false,
-    };
-  },
+    data() {
+        return {
+            lines: this.value.split(/(?:\r\n|\r|\n)/g),
+            isShowMore: false,
+            isMoreVisible: false,
+        };
+    },
 
-  mounted() {
-    if (this.lines.length > 6) { // yes 6, first line is "title"
-      this.isShowMore = true;
-    }
-  },
+    mounted() {
+        if (this.lines.length > 6) { // yes 6, first line is "title"
+            this.isShowMore = true;
+        }
+    },
 };
 </script>
