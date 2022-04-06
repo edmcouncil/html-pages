557e9764daac5d9c8e35efee57f3c3c33c651305
Michał Daniel <michal.19.daniel.96@gmail.com>
2022-04-06 08:57:57 +0200

    Update .eslintrc.js

--- idmp/.eslintrc.js
+++ idmp/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'properties': 0,
-    'ignoreDestructuring': true,
+    'ignoreDestructuring': 0,
     'ignoreImports': false
   },
   parserOptions: {
