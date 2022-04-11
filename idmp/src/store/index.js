b82647e28407cf0172da98d4088bf4742508cb94
Adam <aa.rosolowski@gmail.com>
2022-04-11 11:14:13 +0200

    Add stats element to idmp

--- idmp/src/store/index.js
+++ idmp/src/store/index.js
@@ -10,6 +10,8 @@ export default new Vuex.Store({
     searchDefaultDomain: "/idmp/ontology/{version}api/find",
     ontologyDefaultDomain: "/idmp/ontology/{version}api/entity",
     modulesDefaultDomain: "/idmp/ontology/{version}api/module",
+    statsDefaultDomain: '/idmp/ontology/api/stats',
+    missingImportsDefaultDomain: '/idmp/ontology/api/missingImports',
   },
   mutations: {},
   actions: {},
