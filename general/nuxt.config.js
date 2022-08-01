099b6815b73c022efc132f539974a5d2bd0d58d3
Adam <rr-adam@users.noreply.github.com>
2022-08-01 14:04:34 +0200

    fix multiselect load delay

--- general/nuxt.config.js
+++ general/nuxt.config.js
@@ -76,7 +76,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: "~/plugins/vue-multiselect", mode: "client" },
+    { src: "~/plugins/vue-multiselect" },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
