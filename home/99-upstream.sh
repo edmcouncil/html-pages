6a79d80df1a67c3afe9d45e10883df1a48a388ac
Przemysław Grądzki <przemek.gradzki@gmail.com>
2023-05-26 12:49:06 +0200

    Dynamically set regex for <family> in nginx config
    Signed-off-by: Przemysław Grądzki <przemek.gradzki@gmail.com>

--- home/99-upstream.sh
+++ home/99-upstream.sh
@@ -11,7 +11,7 @@ fi
 
 test -d /strapi/db && export O="$(cd /strapi/db && for i in *.db.template ; do echo ${i/.db.template} ; done | sort | tr [:space:] '|' | sed 's/|$//g')"
 
-test -n "${O}" && for i in $(echo "${O}" | sed 's/|/ /g') ; do
+test -n "${O}" && sed -i "s#\((?<family>\)[^)]*\()\)#\1${O}\2#g" /etc/nginx/conf.d/default.conf && for i in $(echo "${O}" | sed 's/|/ /g') ; do
  ipp="$(getent hosts ${i}-pages | awk '{print $1}')"
  echo -e "upstream ${i}-pages {\n\tserver\t${ipp:-127.0.0.1}:80;\n}"
  ips="$(getent hosts ${i}-strapi | awk '{print $1}')"
@@ -20,5 +20,3 @@ done > /etc/nginx/conf.d/upstream.conf
 
 sed -i "s/\(set\s\+\$html_branch\s\+\)[^\;]\+\(;\)/\1${BRANCH_NAME:-develop}\2/g" /etc/nginx/conf.d/default.conf
 sed -i "s/\(set\s\+\$html_tag\s\+\)[^\;]\+\(;\)/\1${TAG_NAME:-latest}\2/g" /etc/nginx/conf.d/default.conf
-
-# (?<family>auto|esg|fibo|idmp)
