5272656034c61ae85aefe93da5c302447e3d4cf3
Adam <rr-adam@users.noreply.github.com>
2023-06-19 12:56:31 +0200

    Remove test string

--- general/nuxt.config.js
+++ general/nuxt.config.js
@@ -29,7 +29,7 @@ process.env.VUE_BASE_URL =
 
 process.env.VUE_RESOURCES_BASE_URL = process.env.VUE_BASE_URL + process.env.VUE_ONTOLOGY_NAME + "/ontology/";
 
-process.env.STRAPI_URL = "http://edmc-fibo-viewer.dc.makolab.pl:1331" || process.env.STRAPI_URL || "http://localhost:1337";
+process.env.STRAPI_URL = process.env.STRAPI_URL || "http://localhost:1337";
 
 export default {
   // target: 'static' description https://nuxtjs.org/announcements/going-full-static/
