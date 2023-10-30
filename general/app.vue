0692e635afc2897494932a90dc6e0e607a44acec
Adam <rr-adam@users.noreply.github.com>
2023-10-30 11:54:27 +0100

    Rendering changes

--- general/app.vue
+++ general/app.vue
@@ -5,16 +5,3 @@
     </NuxtLayout>
   </div>
 </template>
-
-<script setup>
-  //  Initialize stores
-  import { useConfigurationStore } from './stores/configuration';
-  import { useContentStore } from './stores/content';
-
-  const configurationStore = useConfigurationStore();
-  const contentStore = useContentStore();
-  const runtimeConfig = useRuntimeConfig();
-
-  await useAsyncData('config', () => configurationStore.initialize(runtimeConfig));
-  await useAsyncData('content', () => contentStore.initialize(runtimeConfig));
-</script>
