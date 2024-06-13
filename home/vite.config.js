440d5a45cf52642fe9f2a2238cfaa708897280b3
Przemysław Grądzki <przemek.gradzki@gmail.com>
2024-06-13 14:17:05 +0200

    home/vite.config.js: use BRANCH and TAG variables
    Signed-off-by: Przemysław Grądzki <przemek.gradzki@gmail.com>

--- home/vite.config.js
+++ home/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   const PRODUCT = env.PRODUCT || 'htmlpages';
-  const BRANCH = env.BRANCH_NAME || 'master';
-  const TAG = env.TAG_NAME || 'latest';
+  const BRANCH = env.BRANCH || env.BRANCH_NAME || 'master';
+  const TAG = env.TAG || env.TAG_NAME || 'latest';
 
   return {
     plugins: [
