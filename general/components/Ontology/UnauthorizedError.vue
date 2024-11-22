5dbd493364e3f9c309364e464af82624d6334df2
Adam Rosołowski <87621210+rr-adam@users.noreply.github.com>
2024-11-22 11:42:15 +0100

    Handle unauthorized error and refresh data

--- general/components/Ontology/UnauthorizedError.vue
+++ general/components/Ontology/UnauthorizedError.vue
@@ -3,13 +3,16 @@
     <article>
       <section class="blank not-found-header">
         <p class="big-sign">403</p>
-        <p class="big muted">You don't have access to this resource</p>
+        <p class="big muted">You don't have access to this page</p>
         <p class="muted">Please login and try again.</p>
       </section>
 
       <section>
         <h2>Tips</h2>
-        <p>Try using search box or modules tree to find ontology resources.</p>
+        <p>
+          Try changing version or head to the
+          <nuxt-link to="/">home</nuxt-link> page.
+        </p>
       </section>
     </article>
   </div>
